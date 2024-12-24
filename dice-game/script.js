26caf60d4576a6880f833c4ba4922f787dd2b161
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-24 19:31:51 +0500

    add

--- dice-game/script.js
+++ dice-game/script.js
@@ -3,35 +3,35 @@
 const score0El = document.querySelector('#score--0')
 const score1El = document.querySelector('#score--1')
 const diceEl = document.querySelector('.dice')
-const current0El=document.querySelector('#current--0')
-const current1El=document.querySelector('#current--1')
-const btnNew=document.querySelector('.btn--new')
-const btnRoll=document.querySelector('.btn--roll')
-const btnHold=document.querySelector('.btn--hold')
+const current0El = document.querySelector('#current--0')
+const current1El = document.querySelector('#current--1')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 //boshlanish qisim 
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')//dastlabki kurinishda rasm yuq holatda turadi
 
 
-let currentScore=0
+let currentScore = 0
 //toshni aylantirish funksiyalari
 
-btnRoll.addEventListener('click',function(){
+btnRoll.addEventListener('click', function () {
 	//1.Toshni tasodifiy aylantirish orqali qiymat olishimiz kerak buladi
-	const dice=Math.trunc(Math.random()*6)+1;
-	console.log(dice);
-	
+	const dice = Math.trunc(Math.random() * 6) + 1
+	console.log(dice)
+
 	//2.Toshdan olingan qiymatni ekranda kursatish 
 	diceEl.classList.remove('hidden')
-	diceEl.src=`dice-${dice}.png`
+	diceEl.src = `dice-${dice}.png`
 
 	// 3.Yutqazish funksiyasi ya'ni agar tashlangan tosh 1 ga teng bulsa yani true busa biz uyinni keyingi ishtirokchiga utkazishimiz kerak buladi
 
-	if(dice!==1){
-		currentScore+=dice;
-		current0El.textContent=currentScore
-	}else{
+	if (dice !== 1) {
+		currentScore += dice
+		current0El.textContent = currentScore
+	} else {
 		//Keyingi ishtirokchiga o'tqazish
 	}
 })
