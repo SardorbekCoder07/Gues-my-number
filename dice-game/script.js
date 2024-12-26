64c02e3de19dd43d2e3730324d973e8312d21fc6
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-12-26 09:28:05 +0500

    finish the game

--- dice-game/script.js
+++ dice-game/script.js
@@ -1,37 +1,90 @@
 'use strict'
-//O'zgaruvchilarga saqlangan elementlar
-const score0El = document.querySelector('#score--0')
-const score1El = document.querySelector('#score--1')
+//1.score elementlarini o'zgaruvchiga saqlash
+//2. score elementlarini contentini 0 ga aylantirish 
+//3. dice emementni o'zgaruvchiga saqalab uni displayini nonoe holatiga keltirish 
+//4.roll hamda hold buttonlarni o'zgaruvchiga saqlash va toshni aylanstirish funksiyasini yozish 
+//5. tosh 1 ga teng bo'lganda playerni almashtirish
+//6.currentScore ni saqlash hamda ekranga chiqarish
+//7.score array ochish
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
+const score0El = document.getElementById('score--0')
+const score1El = document.getElementById('score--1')
 const diceEl = document.querySelector('.dice')
-const current0El = document.querySelector('#current--0')
-const current1El = document.querySelector('#current--1')
-const btnNew = document.querySelector('.btn--new')
+const curren0El = document.getElementById('current--0')
+const curren1El = document.getElementById('current--1')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
-//boshlanish qisim 
+const btnNew = document.querySelector('.btn--new')
+
 score0El.textContent = 0
 score1El.textContent = 0
-diceEl.classList.add('hidden')//dastlabki kurinishda rasm yuq holatda turadi
+diceEl.classList.add('hidden')
+
+let score, currentScore, activePlayer, playing
+const init = () => {
+	score = [0, 0]
+	currentScore = 0
+	activePlayer = 0
+	playing = true
+
+	score0El.textContent = 0
+	score1El.textContent = 0
+	curren0El.textContent = 0
+	curren1El.textContent = 0
+
+	diceEl.classList.add('hidden')
+	player0El.classList.remove('player--winner')
+	player1El.classList.remove('player--winner')
+	player0El.classList.add('player--active')
+	player1El.classList.remove('player--active')
+}
+init()
+const switchPlayer = () => {
+	document.getElementById(`current--${activePlayer}`).textContent = 0
+	currentScore = 0
+	activePlayer = activePlayer === 0 ? 1 : 0
+	player0El.classList.toggle('player--active')
+	player1El.classList.toggle('player--active')
+}
+
+btnRoll.addEventListener('click', () => {
+	if (playing) {
+		const dice = Math.trunc(Math.random() * 6) + 1
+		diceEl.classList.remove('hidden')
+		diceEl.src = `dice-${dice}.png`
 
 
-let currentScore = 0
-//toshni aylantirish funksiyalari
-
-btnRoll.addEventListener('click', function () {
-	//1.Toshni tasodifiy aylantirish orqali qiymat olishimiz kerak buladi
-	const dice = Math.trunc(Math.random() * 6) + 1
-	console.log(dice)
-
-	//2.Toshdan olingan qiymatni ekranda kursatish 
-	diceEl.classList.remove('hidden')
-	diceEl.src = `dice-${dice}.png`
-
-	// 3.Yutqazish funksiyasi ya'ni agar tashlangan tosh 1 ga teng bulsa yani true busa biz uyinni keyingi ishtirokchiga utkazishimiz kerak buladi
-
-	if (dice !== 1) {
-		currentScore += dice
-		current0El.textContent = currentScore
-	} else {
-		//Keyingi ishtirokchiga o'tqazish
+		if (dice !== 1) {
+			currentScore += dice
+			document.getElementById(`current--${activePlayer}`).textContent = currentScore
+		} else {
+			switchPlayer()
+		}
 	}
 })
+
+
+btnHold.addEventListener('click', () => {
+	if (playing) {
+		//1. active playerni currentScoreni scorega qo'shish
+		score[activePlayer] += currentScore
+		document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer]
+
+		//score 100 dan baland bulgan active playerni yutdi deb chiqarish
+		if (score[activePlayer] >= 20) {
+			playing = false
+			diceEl.classList.add('hidden')
+			document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+			document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+
+		} else {
+			switchPlayer()
+		}
+		//finish the game
+
+		//score ga qiymat saqlanganda playerni almashtirish
+	}
+})
+
+btnNew.addEventListener('click', init())
